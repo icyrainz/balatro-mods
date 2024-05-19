4e7a2fcfadfc111adaca8d15c317a4ed97e0439c
Tue Phan <icyrainz@gmail.com>
2024-05-19 01:23:26 -0700

    more stuffs

---
+++ copy_mods_linux.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+MOD_PATH="~/.steam/steam/steamapps/compatdata/2379780/pfx/drive_c/users/steamuser/AppData/Roaming/Balatro"
+
+rm -r $MOD_PATH/Mods
+rsync -avzh ~/Github/balatro-mods/Mods $MOD_PATH/
