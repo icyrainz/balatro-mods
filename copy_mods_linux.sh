81c3b27279c5fd72b9813213ddee5529e762158b
Tue Phan <icyrainz@gmail.com>
2024-05-19 01:28:39 -0700

    update script

--- copy_mods_linux.sh
+++ copy_mods_linux.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-MOD_PATH="~/.steam/steam/steamapps/compatdata/2379780/pfx/drive_c/users/steamuser/AppData/Roaming/Balatro"
+MOD_PATH="$HOME/.steam/steam/steamapps/compatdata/2379780/pfx/drive_c/users/steamuser/AppData/Roaming/Balatro"
 
 rm -r $MOD_PATH/Mods
 rsync -avzh ~/Github/balatro-mods/Mods $MOD_PATH/
