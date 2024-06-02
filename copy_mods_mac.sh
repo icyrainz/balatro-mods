295a0dce5ab07ab4d7a78da4fa1dd05c686c8b09
Tue Phan <icyrainz@gmail.com>
2024-06-02 13:24:39 -0700

    move to lovely

--- copy_mods_mac.sh
+++ copy_mods_mac.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-MOD_PATH="$HOME/Library/Application Support/Balatro"
+MY_MODS_PATH="$HOME/Github/balatro-mods/"
+BALATRO_PATH="$HOME/Library/Application Support/Balatro"
+MOD_PATH="$BALATRO_PATH/Mods"
+STEAMODDED_PATH="$MOD_PATH/Steamodded"
 
-rm -r "$MOD_PATH/Mods"
-rsync -avzh ~/Github/balatro-mods/Mods "$MOD_PATH/"
+rm -r "$MOD_PATH"
+mkdir "$MOD_PATH"
+rsync -avzh --exclude '*.git*/*' "$MY_MODS_PATH/Steamodded/" "$STEAMODDED_PATH"
+rsync -avzh --exclude '*.git*/*' "$MY_MODS_PATH/Mods/" "$MOD_PATH"
+
+rm -r "$STEAMODDED_PATH/example_mods"
