0cc59aeaa6c633123b97c1d46d8124a420fc3f6a
Tue Phan <icyrainz@gmail.com>
2024-06-02 13:45:40 -0700

    add SDM0

--- Mods_Repo/copy_mods.sh
+++ Mods_Repo/copy_mods.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
+rm -rf ../Mods
+mkdir ../Mods
+
 cp *.lua ../Mods
-rsync ./FlushHotkeys/steamodded/* ../Mods
+
+# FlushHotkeys
+rsync -avzh ./FlushHotkeys/steamodded/* ../Mods
+
+# SDM0
+rsync -avzh ./SDM0/SDM_0\'s\ Stuff/* ../Mods
--- copy_mods_mac.sh
+++ copy_mods_mac.sh
@@ -5,8 +5,16 @@ BALATRO_PATH="$HOME/Library/Application Support/Balatro"
 MOD_PATH="$BALATRO_PATH/Mods"
 STEAMODDED_PATH="$MOD_PATH/Steamodded"
 
+# Clean up destination
 rm -r "$MOD_PATH"
 mkdir "$MOD_PATH"
+
+# Regenerate source
+cd ./Mods_Repo/
+sh ./copy_mods.sh
+cd -
+
+# Sync files
 rsync -avzh --exclude '*.git*/*' "$MY_MODS_PATH/Steamodded/" "$STEAMODDED_PATH"
 rsync -avzh --exclude '*.git*/*' "$MY_MODS_PATH/Mods/" "$MOD_PATH"
 
